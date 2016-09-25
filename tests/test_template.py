75064399e7e72fd67e2a0c21c675d6289e7d1ec9
Graham Higgins <gjh-github@bel-epa.com>
2016-09-25 04:49:30 +0100

    Added test of file read

--- tests/test_template.py
+++ tests/test_template.py
@@ -2,18 +2,28 @@
 import unittest
 import platform
 import doctest
+import sys
+import os
+from tempita import Template
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 
 class TestTempita(unittest.TestCase):
     """Dicstring."""
-    def test_template(self):
+    def test_doctest_template(self):
         if platform.python_implementation == 'CPython':
             doctest.testfile('test_template{}.txt'.format(sys.version))
         else:
             doctest.testfile('test_template.pypytxt')
 
-    def test_template(self):
+    def test_doctest_docs_index(self):
         doctest.testfile('../docs/index.txt')
+
+    def test_read_template_from_file(self):
+        filename = '/tests/test_basetemplate.txt'
+        namespace = dict(name="Arthur Dent")
+        t = Template.from_filename(sys.path[0] + filename, namespace=namespace, encoding="latin-1")
+        print(t)
 
 if __name__ == "__main__":
     unittest.main()
